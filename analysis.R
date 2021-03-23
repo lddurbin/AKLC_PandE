a4f59d3513cdd9d7f11c3e998b348157f8c9fecd
Lee Durbin <l.d.durbin@gmail.com>
2021-03-23 00:27:03 +0000

    Create location and content columns using unite(). Complete analysis.

--- analysis.R
+++ analysis.R
@@ -1,6 +1,7 @@
 library("dplyr")
 library("janitor")
 library("readxl")
+library("tidyr")
 
 df <- read_excel("P_and_E_data.xlsx") %>% 
   clean_names() %>% 
@@ -9,11 +10,16 @@ df <- read_excel("P_and_E_data.xlsx") %>%
       where_was_the_session_delivered == "Community library" ~ "Inside a library",
       where_was_the_session_delivered != "Community library" ~ "Outside a library"
     ),
+    location = case_when(
+      !is.na(in_which_community_library_was_the_session_delivered) ~ in_which_community_library_was_the_session_delivered,
+      !is.na(in_which_community_place_venue_was_the_session_delivered) ~ in_which_community_place_venue_was_the_session_delivered,
+      !is.na(where_was_the_session_delivered) ~ where_was_the_session_delivered,
+      !is.na(how_was_the_session_delivered) ~ how_was_the_session_delivered
+    ),
     delivery_group = case_when(
       was_the_session_co_delivered_with_other_library_teams_or_other_partners == "Yes" ~ "Co-delivered",
       who_delivered_the_session == "The library’s team" ~ "Library",
       who_delivered_the_session != "The library's team" ~ "External"
     )
-  )
-
-
+  ) %>% 
+  unite("content", c(42:57), na.rm = TRUE)
