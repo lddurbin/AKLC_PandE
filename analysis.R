ab33d218f781a255b98e32a3759f0cef2b37a619
Lee Durbin <l.d.durbin@gmail.com>
2021-03-24 00:34:33 +0000

    Complete analysis, ensure "Other" options are showing as expected.

--- analysis.R
+++ analysis.R
@@ -1,10 +1,13 @@
-library("dplyr")
+library("tidyverse")
 library("janitor")
 library("readxl")
-library("tidyr")
+
+content_index <- read_csv("content_index.csv", col_types = "c") %>% mutate(content = str_trim(content) %>% str_to_lower())
+other_communities <- read_csv("other_communities.csv", col_types = "c")
 
 df <- read_excel("P_and_E_data.xlsx") %>% 
   clean_names() %>% 
+  unite("content", c(42:57), na.rm = TRUE) %>% 
   mutate(
     location_is_library = case_when(
       where_was_the_session_delivered == "Community library" ~ "Inside a library",
@@ -20,6 +23,13 @@ df <- read_excel("P_and_E_data.xlsx") %>%
       was_the_session_co_delivered_with_other_library_teams_or_other_partners == "Yes" ~ "Co-delivered",
       who_delivered_the_session == "The library’s team" ~ "Library",
       who_delivered_the_session != "The library's team" ~ "External"
+    ),
+    content = case_when(
+      content == "Arts & Crafts" ~ "arts & craft",
+      !is.na(content) ~ str_trim(content) %>% str_to_lower()
+    ),
+    content_other = case_when(
+      content %in% content_index$content ~ "Selection",
+      !content %in% content_index$content ~ "Other",
     )
-  ) %>% 
-  unite("content", c(42:57), na.rm = TRUE)
+  )
